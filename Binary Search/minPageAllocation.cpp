86bca276a666125b323d2edbe3fce76f6c8fb33d
kisho1131 <sumitkishore0131@gmail.com>
2021-11-14 20:43:26 +0530

    Binary Search

--- Binary Search/minPageAllocation.cpp
+++ Binary Search/minPageAllocation.cpp
@@ -45,3 +45,24 @@ int main(int argc, char const *argv[]) {
   cout << endl;
   return 0;
 }
+
+/* Similar Questions --> To be Solved
+---------------------------------------
+Book Allocation Problem (GFG)
+Aggressive cow (spoj)
+Prata and roti (spoj)
+EKO (spoj)
+Painter Partition Problem
+Google kickstart A Q-3 2020
+
+LeetCode:
+---------
+1482 Minimum Number of Days to Make m Bouquets
+1283 Find the Smallest Divisor Given a Threshold
+1231 Divide Chocolate
+1011 Capacity To Ship Packages In N Days
+875 Koko Eating Bananas Minimize
+774 Max Distance to Gas Station
+410 Split Array Largest Sum
+
+*/
