e2bb6d344a1b959322e815f768c244805acef04c
Sumit Kumar <sumitkishore0131@gmail.com>
2022-02-15 00:13:20 +0530

    # recursion - sortArray

--- Recursion/sortArray.cpp
+++ Recursion/sortArray.cpp
@@ -32,3 +32,26 @@ int main(int argc, char const *argv[]) {
   cout << endl;
   return 0;
 }
+
+/*
+
+void Insert(vector<int>&arr, int lastval){
+  if(arr.size() == 0 || arr[arr.size()-1] <= lastval){
+    arr.push_back(lastval);
+    reurn;
+  }
+  int ele = arr[arr.size()-1];
+  arr.pop_back();
+  Insert(arr, lastval);
+  arr.push_back(ele);
+}
+
+void sort(vector<int>arr){
+  if(arr.size() == 0) return;
+  int lastval = arr[arr.size()-1];
+  arr.pop_back();
+  sort(arr);
+  Insert(arr, lastval);
+}
+
+*/
